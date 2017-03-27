0d9317f63fac0da79b6b8f276f7947f04998f755
railsbeginner <bartosz.dyja@gmail.com>
2017-03-27 22:01:54 +0200

    Migrate article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327192146) do
+ActiveRecord::Schema.define(version: 20170327200023) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friends", force: :cascade do |t|
     t.string   "first_name"
