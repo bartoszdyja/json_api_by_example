1699010bf36aecc1a48b77c36a4ef99dc275bfb6
railsbeginner <bartosz.dyja@gmail.com>
2017-03-27 22:11:12 +0200

    Add loan resource and controller

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,4 +1,5 @@
 class Loan < ApplicationRecord
   belongs_to :friend
   belongs_to :article
+  validates :friend, :article, presence: true
 end
---
+++ app/resources/loan_resource.rb
@@ -0,0 +1,5 @@
+class LoanResource < JSONAPI::Resource
+  attributes :notes, :returned
+  has_one :article
+  has_one :friend
+end
